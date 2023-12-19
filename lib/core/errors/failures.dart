fb46e2972ed523e5ab9867dd7e649cc19a108801
andikatp <triprasetya_andika@yahoo.com>
2023-12-19 19:34:21 +0700

    creating on boarding, adding exception, failures, and repository.

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:teacher/core/errors/exceptions.dart';
+
+abstract class Failure extends Equatable {
+  const Failure({required this.message, required this.statusCode});
+  final String message;
+  final int statusCode;
+
+  String get errorMessage => '$statusCode Error: $message';
+
+  @override
+  List<Object?> get props => [message, statusCode];
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure({required super.message, required super.statusCode});
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure({required super.message, required super.statusCode});
+
+  ServerFailure.fromException(ServerException exception)
+      : this(message: exception.message, statusCode: exception.statusCode);
+}
