a9086b695865e1c25c97228faa1f5f371a16735a
andikatp <triprasetya_andika@yahoo.com>
2024-01-09 19:58:54 +0700

    router

---
+++ lib/core/common/app/providers/user_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:teacher/features/auth/data/models/local_user_model.dart';
+
+class UserProvider extends ChangeNotifier {
+  LocalUserModel? _user;
+
+  LocalUserModel? get user => _user;
+
+  void initUser(LocalUserModel? user) {
+    if (user != null) _user = user;
+  }
+
+  set user(LocalUserModel? user) {
+    if (_user != user) {
+      _user = user;
+      Future.delayed(Duration.zero, notifyListeners);
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:teacher/core/common/app/providers/user_provider.dart';
 import 'package:teacher/core/res/colours.dart';
 import 'package:teacher/core/res/fonts.dart';
 import 'package:teacher/core/services/injection_container.dart';
@@ -20,14 +22,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: Fonts.poppins,
-        appBarTheme: const AppBarTheme(color: Colors.transparent),
-        colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
+    return ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      child: MaterialApp(
+        theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: Fonts.poppins,
+          appBarTheme: const AppBarTheme(color: Colors.transparent),
+          colorScheme:
+              ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
+        ),
+        onGenerateRoute: generateRoute,
       ),
-      onGenerateRoute: generateRoute,
     );
   }
 }
