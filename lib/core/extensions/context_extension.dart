055e3801cf86aef5ae9eacb4763d4baa73ae126b
andikatp <triprasetya_andika@yahoo.com>
2024-01-11 13:18:28 +0700

    context extension

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:teacher/core/common/app/providers/tab_navigator.dart';
 import 'package:teacher/core/common/app/providers/user_provider.dart';
 import 'package:teacher/features/auth/domain/entities/user_entity.dart';
 
@@ -13,6 +14,12 @@ extension ContextExt on BuildContext {
   double get height => size.height;
 
   UserProvider get userProvider => read<UserProvider>();
-  
+
   LocalUser? get currentUser => userProvider.user;
+
+  TabNavigator get tabNavigator => read<TabNavigator>();
+
+  void pop() => tabNavigator.pop();
+
+  void push(Widget page) => tabNavigator.push(TabItem(child: page));
 }
